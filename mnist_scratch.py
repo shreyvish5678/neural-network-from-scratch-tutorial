bd02b273addcde6b1f3747a34992035fcc8bc1bd
shreyvish5678 <129219936+shreyvish5678@users.noreply.github.com>
2023-08-10 03:49:10 -0700

    Update mnist_scratch.py

--- mnist_scratch.py
+++ mnist_scratch.py
@@ -1,7 +1,5 @@
 from tensorflow.keras import datasets 
-import numpy as np 
-from PIL import Image 
-import matplotlib.pyplot as plt 
+import numpy as np  
 (X_train, y_train), (X_test, y_test) = datasets.mnist.load_data()
 X_train = X_train.reshape(X_train.shape[0], -1)
 X_test = X_test.reshape(X_test.shape[0], -1)
@@ -49,22 +47,29 @@ class loss_catergorical_crossentropy(Loss):
     return negative_log
 
 class activation_softmax_loss_catergorical_crossentropy():
-    def __init__(self):
-        self.activation = activation_Softmax()
-        self.loss = loss_catergorical_crossentropy()
+  def __init__(self):
+    self.activation = activation_Softmax()
+    self.loss = loss_catergorical_crossentropy()
 
-    def forward(self, inputs, y_true):
-        self.activation.forward(inputs)
-        self.output = self.activation.output
-        return self.loss.calculate(self.output, y_true)
+  def forward(self, inputs, y_true):
+    self.activation.forward(inputs)
+    self.output = self.activation.output
+    return self.loss.calculate(self.output, y_true)
 
-    def backprop(self, dvalues, y_true):
-        samples = len(dvalues)
-        if len(y_true.shape) == 2:
-            y_true = np.argmax(y_true, axis=1)
-        self.dinputs = dvalues.copy()
-        self.dinputs[range(samples), y_true] -= 1
-        self.dinputs = self.dinputs / samples
+  def backprop(self, dvalues, y_true):
+    samples = len(dvalues)
+    if len(y_true.shape) == 2:
+      y_true = np.argmax(y_true, axis=1)
+    self.dinputs = dvalues.copy()
+    self.dinputs[range(samples), y_true] -= 1
+    self.dinputs = self.dinputs / samples
+
+class Optimizer_SGD:
+  def __init__(self, learning_rate):
+    self.learning_rate = learning_rate
+  def update_params(self, layer):
+    layer.weights += -self.learning_rate * layer.dweights
+    layer.biases += -self.learning_rate * layer.dbiases
 
 dense1 = layer_Dense(784, 128)
 relu1 = activation_ReLu()
@@ -73,43 +78,45 @@ relu2 = activation_ReLu()
 dense3 = layer_Dense(64, 10)
 
 softmax_loss = activation_softmax_loss_catergorical_crossentropy() 
+optimizer = Optimizer_SGD(learning_rate=0.001)
 
-epochs = 10 
-batch_size = 32 
-learning_rate = 0.001 
+epochs = 20 
+batch_size = 32  
 
 for epoch in range(epochs): 
-    indices = np.arange(len(X_train)) 
-    np.random.shuffle(indices) 
-    X_train = X_train[indices] 
-    y_train = y_train[indices]
+  indices = np.arange(len(X_train)) 
+  np.random.shuffle(indices) 
+  X_train = X_train[indices] 
+  y_train = y_train[indices]
     
-    for i in range(0, len(X_train), batch_size):
-        X_batch = X_train[i:i+batch_size] 
-        y_batch = y_train[i:i+batch_size] 
+  for i in range(0, len(X_train), batch_size):
+    X_batch = X_train[i:i+batch_size] 
+    y_batch = y_train[i:i+batch_size] 
         
-        dense1.forward(X_batch)
-        relu1.forward(dense1.output)
-        dense2.forward(relu1.output)
-        relu2.forward(dense2.output)
-        dense3.forward(relu2.output)
-        loss = softmax_loss.forward(dense3.output, y_batch)
-        
-        softmax_loss.backprop(softmax_loss.output, y_batch)
-        dense3.backprop(softmax_loss.dinputs)
-        relu2.backprop(dense3.dinputs)
-        dense2.backprop(relu2.dinputs)
-        relu1.backprop(dense2.dinputs)
-        dense1.backprop(relu1.dinputs)
+    dense1.forward(X_batch)
+    relu1.forward(dense1.output)
+    dense2.forward(relu1.output)
+    relu2.forward(dense2.output)
+    dense3.forward(relu2.output)
+    loss = softmax_loss.forward(dense3.output, y_batch)
 
-        dense1.weights -= learning_rate * dense1.dweights
-        dense1.biases -= learning_rate * dense1.dbiases
-        dense2.weights -= learning_rate * dense2.dweights
-        dense2.biases -= learning_rate * dense2.dbiases
-        dense3.weights -= learning_rate * dense3.dweights
-        dense3.biases -= learning_rate * dense3.dbiases
+    predictions = np.argmax(softmax_loss.output, axis=1)
+    if len(y_batch.shape) == 2:
+      y_batch = np.argmax(y_batch, axis=1)
+    accuracy = np.mean(predictions == y_batch)
         
-    print(f'Epoch {epoch+1}/{epochs}, Loss: {loss:.4f}')
+    softmax_loss.backprop(softmax_loss.output, y_batch)
+    dense3.backprop(softmax_loss.dinputs)
+    relu2.backprop(dense3.dinputs)
+    dense2.backprop(relu2.dinputs)
+    relu1.backprop(dense2.dinputs)
+    dense1.backprop(relu1.dinputs)
+
+    optimizer.update_params(dense1)
+    optimizer.update_params(dense2)
+    optimizer.update_params(dense3)
+        
+  print(f'Epoch {epoch+1}/{epochs}, Loss: {loss:.4f}, Accuracy: {accuracy:.4f}, LR: {optimizer.current_learning_rate:.5f}')
 
 dense1.forward(X_test)
 relu1.forward(dense1.output)
@@ -121,8 +128,13 @@ test_loss = softmax_loss.forward(dense3.output, y_test)
 print(f'Test Loss: {test_loss:.4f}') 
 
 predictions = np.argmax(softmax_loss.output, axis=1) 
+if len(y_test.shape) == 2: 
+  y_test = np.argmax(y_test, axis=1) 
 accuracy = np.mean(predictions == y_test)
 print(f'Test Accuracy: {accuracy * 100:.2f}%')
+
+from PIL import Image 
+import matplotlib.pyplot as plt
 
 def predict_image(img_path):
   img_path = img_path
